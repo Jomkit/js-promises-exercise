8dcac185b415b851a97947fed4198048f8802aad
Jomkit <j.jujaroen96@gmail.com>
2024-02-28 18:06:04 +0700

    refactored cards.js and numbers.js for async/await functionality

--- static/numbers.js
+++ static/numbers.js
@@ -1,48 +1,50 @@
 /****** Number Facts *******/
-const num_url = 'http://numbersapi.com';
+const baseURL = 'http://numbersapi.com';
 
 // 1. Make requests to Numbers API to get a fact about your favorite number
 // Make sure response is JSON
 
-let numPromise = axios.get(`${num_url}/5?json`)
-    .then(res => {
-        p = $('#fav-num');
-        text = $(`<p>Fun fact: ${res.data.text}</p>`);
-        p.append(text[0]);
-        console.log(res.data.text);
-    });
+async function part1(){
+    let res = await axios.get(`${baseURL}/5?json`)
+    // console.log(res.data);
+    p = $('#fav-num');
+    text = $(`<p>Fun fact: ${res.data.text}</p>`);
+    p.append(text[0]);
+}
+part1();
 
 // 2. Figure out how to get data on multiple numbers in a single request. Make that 
 // request and when you get the data back, put all of the number facts on the page.
 
-let num2Promise = axios.get(`${num_url}/11..15?json`)
-    .then(res => {
-        p = $('#mult-num');
-        for(num in res.data){
-            fact = JSON.stringify(res.data[num]);
-            text = $(`<p>${num}: ${fact}</p>`);
-            p.append(text[0]);
+async function part2(){
+    let {data: p2} = await axios.get(`${baseURL}/11..15?json`);
+    p = $('#mult-num');
+    for(num in p2){
+        fact = JSON.stringify(p2[num]);
+        text = $(`<p>${num}: ${fact}</p>`);
+        p.append(text[0]);
 
-        } 
-        
-    });
+    } 
+    // console.log(p2);
+}
+part2();
 
 // 3. Use the API to get 4 facts on your favorite number. Once you have them all, 
 // put them on the page. It’s okay if some of the facts are repeats.
-let fourNumPromises = [];
 
-for(let i = 0; i < 4; i++){
-    fourNumPromises.push(
-        axios.get(`${num_url}/5?json`)
-    );
+async function part3(){
+    let fourFacts = await Promise.all([
+        axios.get(`${baseURL}/5?json`),
+        axios.get(`${baseURL}/5?json`),
+        axios.get(`${baseURL}/5?json`),
+        axios.get(`${baseURL}/5?json`)
+    ]);
+    p = $('#fav-num');
+    p.append('<h2>More facts about my favorite number:</h2>');
+    // console.log(fourFacts[0].data.text);
+    for(i in fourFacts){
+        let text = $(`<p>${fourFacts[i].data.text}</p>`);
+        p.append(text);
+    }
 }
-
-Promise.all(fourNumPromises)
-    .then(numArr => {
-        p = $('#fav-num');
-        p.append(`<h2>More facts about my favorite number:</h2>`)
-        numArr.forEach(n => {
-            text = $(`<p>${n.data.text}</p>`);
-            p.append(text[0]);
-        })
-    });
+part3();
